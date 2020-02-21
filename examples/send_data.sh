4e44e162a3ab75b639c91aebca04adcd5d8ceb17
Nathan Lesné <nathan.lesne@etu.u-bordeaux.fr>
2020-02-21 17:12:20 +0100

    Added bash script to send data at an interval for debbugging purposes

---
+++ examples/send_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PORT=52001
+echo "What do you want to send?"
+read value
+echo "Sending $value every second to port $PORT"
+echo "Press CTRL-C to stop."
+while echo value;
+ do 
+    sleep 1
+ done | nc -u localhost $PORT
+
