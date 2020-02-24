8d4d7f70120d7436a4e5a2053c94141660a75812
Nathan Lesné <nathan.lesne@etu.u-bordeaux.fr>
2020-02-24 16:32:24 +0100

    fixed auto sending script

--- examples/send_data.sh
+++ examples/send_data.sh
@@ -4,7 +4,7 @@ echo "What do you want to send?"
 read value
 echo "Sending $value every second to port $PORT"
 echo "Press CTRL-C to stop."
-while echo value;
+while echo $value;
  do 
     sleep 1
  done | nc -u localhost $PORT
