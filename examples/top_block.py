cbfafb619c19d59e7c33ba3d16131821d0c94e2a
Luka M <luka.moraiz@etu.u-bordeaux.fr>
2020-04-05 16:57:45 +0200

    modif emplacement GUI

--- examples/top_block.py
+++ examples/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Fri Apr  3 18:35:18 2020
+# Generated: Sun Apr  5 16:54:26 2020
 ##################################################
 
 from distutils.version import StrictVersion
@@ -24,16 +24,13 @@ from PyQt5.QtCore import QObject, pyqtSlot
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
-from gnuradio import uhd
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
-from gnuradio.filter import pfb
 from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
 import lora
 import math
 import sys
-import time
 from gnuradio import qtgui
 
 
@@ -70,9 +67,9 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.tx_gain = tx_gain = 0
         self.spreading_factor = spreading_factor = 8
         self.samp_rate = samp_rate = 250e3
+        self.rx_gain = rx_gain = 0
         self.offset = offset = 0
         self.ldr = ldr = True
         self.header = header = False
@@ -83,19 +80,23 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self._tx_gain_range = Range(0, 50, 1, 0, 200)
-        self._tx_gain_win = RangeWidget(self._tx_gain_range, self.set_tx_gain, 'Gain', "counter", int)
-        self.top_grid_layout.addWidget(self._tx_gain_win, 1, 1, 1, 1)
-        [self.top_grid_layout.setRowStretch(r,1) for r in range(1,2)]
-        [self.top_grid_layout.setColumnStretch(c,1) for c in range(1,2)]
         self._spreading_factor_range = Range(7, 12, 1, 8, 200)
         self._spreading_factor_win = RangeWidget(self._spreading_factor_range, self.set_spreading_factor, "spreading_factor", "counter_slider", int)
         self.top_layout.addWidget(self._spreading_factor_win)
+        self._rx_gain_range = Range(0, 50, 1, 0, 200)
+        self._rx_gain_win = RangeWidget(self._rx_gain_range, self.set_rx_gain, 'Gain', "counter", int)
+        self.top_grid_layout.addWidget(self._rx_gain_win, 0, 1, 1, 1)
+        [self.top_grid_layout.setRowStretch(r,1) for r in range(0,1)]
+        [self.top_grid_layout.setColumnStretch(c,1) for c in range(1,2)]
         self._offset_range = Range(-samp_rate/2, samp_rate/2, 1000, 0, 200)
         self._offset_win = RangeWidget(self._offset_range, self.set_offset, 'offset', "counter", float)
-        self.top_grid_layout.addWidget(self._offset_win, 2, 0, 1, 1)
-        [self.top_grid_layout.setRowStretch(r,1) for r in range(2,3)]
+        self.top_grid_layout.addWidget(self._offset_win, 1, 0, 1, 1)
+        [self.top_grid_layout.setRowStretch(r,1) for r in range(1,2)]
         [self.top_grid_layout.setColumnStretch(c,1) for c in range(0,1)]
+        self.lora_mod_0 = lora.mod(spreading_factor, 0x12)
+        self.lora_encode_0 = lora.encode(spreading_factor, code_rate, ldr, header)
+        self.lora_demod_0 = lora.demod(spreading_factor, ldr, 25.0, 2)
+        self.lora_decode_0 = lora.decode(spreading_factor, code_rate, ldr, header)
         self._frequency_options = (868.1e6, 433e6, )
         self._frequency_labels = ('868.1 Mhz', '433 Mhz', )
         self._frequency_group_box = Qt.QGroupBox('Frequency')
@@ -116,57 +117,34 @@ class top_block(gr.top_block, Qt.QWidget):
         self._frequency_callback(self.frequency)
         self._frequency_button_group.buttonClicked[int].connect(
         	lambda i: self.set_frequency(self._frequency_options[i]))
-        self.top_grid_layout.addWidget(self._frequency_group_box, 2, 1, 1, 1)
-        [self.top_grid_layout.setRowStretch(r,1) for r in range(2,3)]
+        self.top_grid_layout.addWidget(self._frequency_group_box, 1, 1, 1, 1)
+        [self.top_grid_layout.setRowStretch(r,1) for r in range(1,2)]
         [self.top_grid_layout.setColumnStretch(c,1) for c in range(1,2)]
         self._bw_range = Range(0, 150000, 1000, 125000, 200)
         self._bw_win = RangeWidget(self._bw_range, self.set_bw, 'bw', "counter", int)
-        self.top_grid_layout.addWidget(self._bw_win, 1, 0, 1, 1)
-        [self.top_grid_layout.setRowStretch(r,1) for r in range(1,2)]
+        self.top_grid_layout.addWidget(self._bw_win, 0, 0, 1, 1)
+        [self.top_grid_layout.setRowStretch(r,1) for r in range(0,1)]
         [self.top_grid_layout.setColumnStretch(c,1) for c in range(0,1)]
-        self.uhd_usrp_sink_0 = uhd.usrp_sink(
-        	",".join(('', "")),
-        	uhd.stream_args(
-        		cpu_format="fc32",
-        		channels=range(1),
-        	),
-        )
-        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_sink_0.set_center_freq(frequency, 0)
-        self.uhd_usrp_sink_0.set_gain(tx_gain, 0)
-        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
-        self.pfb_arb_resampler_xxx_0 = pfb.arb_resampler_ccf(
-        	  samp_rate/bw,
-                  taps=None,
-        	  flt_size=32)
-        self.pfb_arb_resampler_xxx_0.declare_sample_delay(0)
-
-        self.lora_mod_0 = lora.mod(spreading_factor, 0x12)
-        self.lora_encode_0 = lora.encode(spreading_factor, code_rate, ldr, header)
+        self.blocks_throttle_0_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.blocks_socket_pdu_0_0 = blocks.socket_pdu("UDP_CLIENT", '127.0.0.1', '52002', 10000, False)
         self.blocks_socket_pdu_0 = blocks.socket_pdu("UDP_SERVER", '127.0.0.1', '52001', 10000, False)
-        self.blocks_rotator_cc_0 = blocks.rotator_cc((2 * math.pi * offset) / samp_rate)
+        self.blocks_message_debug_0 = blocks.message_debug()
 
         ##################################################
         # Connections
         ##################################################
         self.msg_connect((self.blocks_socket_pdu_0, 'pdus'), (self.lora_encode_0, 'in'))
+        self.msg_connect((self.lora_decode_0, 'out'), (self.blocks_message_debug_0, 'print_pdu'))
+        self.msg_connect((self.lora_decode_0, 'out'), (self.blocks_socket_pdu_0_0, 'pdus'))
+        self.msg_connect((self.lora_demod_0, 'out'), (self.lora_decode_0, 'in'))
         self.msg_connect((self.lora_encode_0, 'out'), (self.lora_mod_0, 'in'))
-        self.connect((self.blocks_rotator_cc_0, 0), (self.pfb_arb_resampler_xxx_0, 0))
-        self.connect((self.lora_mod_0, 0), (self.blocks_rotator_cc_0, 0))
-        self.connect((self.pfb_arb_resampler_xxx_0, 0), (self.uhd_usrp_sink_0, 0))
+        self.connect((self.blocks_throttle_0_0, 0), (self.lora_demod_0, 0))
+        self.connect((self.lora_mod_0, 0), (self.blocks_throttle_0_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
-
-    def get_tx_gain(self):
-        return self.tx_gain
-
-    def set_tx_gain(self, tx_gain):
-        self.tx_gain = tx_gain
-        self.uhd_usrp_sink_0.set_gain(self.tx_gain, 0)
-
 
     def get_spreading_factor(self):
         return self.spreading_factor
@@ -179,16 +157,19 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
-        self.pfb_arb_resampler_xxx_0.set_rate(self.samp_rate/self.bw)
-        self.blocks_rotator_cc_0.set_phase_inc((2 * math.pi * self.offset) / self.samp_rate)
+        self.blocks_throttle_0_0.set_sample_rate(self.samp_rate)
+
+    def get_rx_gain(self):
+        return self.rx_gain
+
+    def set_rx_gain(self, rx_gain):
+        self.rx_gain = rx_gain
 
     def get_offset(self):
         return self.offset
 
     def set_offset(self, offset):
         self.offset = offset
-        self.blocks_rotator_cc_0.set_phase_inc((2 * math.pi * self.offset) / self.samp_rate)
 
     def get_ldr(self):
         return self.ldr
@@ -208,7 +189,6 @@ class top_block(gr.top_block, Qt.QWidget):
     def set_frequency(self, frequency):
         self.frequency = frequency
         self._frequency_callback(self.frequency)
-        self.uhd_usrp_sink_0.set_center_freq(self.frequency, 0)
 
     def get_code_rate(self):
         return self.code_rate
@@ -221,7 +201,6 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_bw(self, bw):
         self.bw = bw
-        self.pfb_arb_resampler_xxx_0.set_rate(self.samp_rate/self.bw)
 
 
 def main(top_block_cls=top_block, options=None):
